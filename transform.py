535019f24268cc1d72f249a56d26607c52e43bf5
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2020-02-22 00:32:50 +0330

    Model selection --> Grid Search

--- transform.py
+++ transform.py
@@ -3,6 +3,13 @@ from sklearn.preprocessing import MinMaxScaler
 
 class TargetTransform:
     def __init__(self, data, gamma=1e-3):
+        r"""Helper calss for transforming and inverse transforming a set of skewed data.
+
+        Parameters:
+        --------------------
+            data: array-like set for extracting min and max. These values will be used for normalization.
+            gamma (float): very small number for adding to data to avoid error when take log of zero values.
+        """
         self.data = self.__reshape2d(data)
         self.scaler = MinMaxScaler()
         self.scaler.fit(self.data)
@@ -22,11 +29,31 @@ class TargetTransform:
 
 
     def transform(self, arr):
+        r"""Log-Transform data and then normalize them using MinMaxScaler
+
+        Parameter:
+        ---------------------
+            arr: array-like that should be transformed.
+
+        Returns:
+        ---------------------
+            numpy.ndarray: transformed data.
+        """
         arr = self.__reshape2d(arr)
         arr = np.log(arr + self.gamma)
         return self.__reshape1d(self.scaler.transform(arr))
 
     def inverse(self, arr):
+        r"""Inverese-Transform data using same scaler and gamma.
+
+        Parameter:
+        ---------------------
+            arr: array-like that should be inverse-transformed.
+
+        Returns:
+        ---------------------
+            numpy.ndarray: inverse-transformed data.
+        """
         arr = self.__reshape2d(arr)
         arr = self.scaler.inverse_transform(arr)
         arr = np.exp(arr)
